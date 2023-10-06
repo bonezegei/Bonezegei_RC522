239f4056c4d302873ab67c97312476b0998c23c8
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2023-10-06 10:29:48 +0800

    Update Bonezegei_RC522.h

--- src/Bonezegei_RC522.h
+++ src/Bonezegei_RC522.h
@@ -6,11 +6,18 @@
 
 #ifndef _BONEZEGEI_RC522_H_
 #define _BONEZEGEI_RC522_H_
-#include <Arduino.h>
+#include <Arduino.h
+#include <SPI.h>
 
+static const int RC522_SPISPEED = 1000000;  // 80 MHz
 class Bonezegei_RC522 {
 public:
-  Bonezegei_RC522();
+  Bonezegei_RC522(int ss, int rst);
+
+  void begin();
+
+  char _ss, _rst;
+  SPIClass *vspi = NULL;
 };
 
 #endif
