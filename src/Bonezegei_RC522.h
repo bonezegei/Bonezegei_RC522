2dde71087bbb8afb855251e365290effa50340e6
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2023-10-06 08:57:36 +0800

    Update Bonezegei_RC522.h

--- src/Bonezegei_RC522.h
+++ src/Bonezegei_RC522.h
@@ -1,1 +1,16 @@
+/*
+  This Library is written for Bonezegei RC522
+  Author: Bonezegei (Jofel Batutay)
+  Date: October 2023 
+*/
 
+#ifndef _BONEZEGEI_RC522_H_
+#define _BONEZEGEI_RC522_H_
+#include <Arduino.h>
+
+class Bonezegei_RC522 {
+public:
+  Bonezegei_RC522();
+};
+
+#endif
